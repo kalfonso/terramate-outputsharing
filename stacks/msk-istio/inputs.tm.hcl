fb9d5097c2a80f4b6be6fa2a1ac00d690c1f40fa
Karel Alfonso <kalfonso@squareup.com>
2024-11-20 23:28:25 -0800

    fix inputs

--- stacks/msk-istio/inputs.tm.hcl
+++
@@ -1,5 +0,0 @@
-input "brokers" {
-  backend       = "default"
-  from_stack_id = "msk"
-  value         = outputs.brokers.value
-}
---
+++ stacks/msk/sharing_generated.tf
@@ -0,0 +1,6 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+output "brokers" {
+  value     = local.brokers
+  sensitive = false
+}
